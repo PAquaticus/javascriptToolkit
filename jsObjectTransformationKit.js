7f6f7ae1741f9719f691254e0c1af729af538b40
Pascal <pascalbawidamannqgooglemail.com>
2018-08-05 15:47:53 +0200

    added some comments and did refactoring

--- jsObjectTransformationKit.js
+++ jsObjectTransformationKit.js
@@ -1,44 +1,69 @@
-function rerootElements(objectRoot, rerootables, nameOfNewroot){
-  parentsNames = Object.keys(objectRoot); 
-  for(parentIndex = 0; parentIndex < parentsNames.length; parentIndex++){
-    var parentObject = objectRoot[parentsNames[parentIndex]];
-    if (typeof parentObject == "object"){
+/**
+ * Iterates over elements in object and if they are element of the
+ * 'rerootables' packs them as children of Newroot object
+ * Example:
+ * {
+ * a : "rerootme1",
+ * b : "rerootme2",
+ * c : "not Element of rerootables"
+ * }
+ * rerootables = ['a', 'b'], nameOfNewroot = "ab"
+ * {
+ * ab : {
+ *        a : "rerootme1",
+ *        b : "rerootme2"
+ *      }
+ * c : "not Element of rerootables"
+ * }
+ * @param  {object} objectRoot
+ * @param  {array} rerootables
+ * @param  {string} nameOfNewroot
+ * @return {object} objectRoot
+ */
+function rerootElements(objectRoot, rerootables, nameOfNewroot) {
+  parentNames = Object.keys(objectRoot);
+
+  for (parentIndex = 0; parentIndex < parentNames.length; parentIndex++) {
+    parentObject = objectRoot[parentNames[parentIndex]];
+    if (typeof parentObject == 'object') {
       keys = Object.keys(parentObject);
-      var newObject = {};
-      var newroot = {}; 
-      for (i=0; i<keys.length; i++){
-        if (rerootables.includes(keys[i])){
+      newObject = {};
+      newroot = {};
+      for (i=0; i<keys.length; i++) {
+        if (rerootables.includes(keys[i])) {
           newroot[keys[i]] = parentObject[keys[i]];
-        }
-        else {
+        } else {
           newObject[keys[i]] = parentObject[keys[i]];
         }
-      }    
-      if (Object.keys(newroot).length > 0){
+      }
+      if (Object.keys(newroot).length > 0) {
         newObject[nameOfNewroot] = newroot;
-        objectRoot[parentsNames[parentIndex]] = newObject; 
+        objectRoot[parentNames[parentIndex]] = newObject;
       }
     }
   }
   return objectRoot;
 }
 
-// testing area - TODO: refactor this into a proper testing file
-var rerootables= ["a","b","c"];
-var dummyobject = {parent: {
-      a : "stu", 
-      b : "stub", 
-      c : "stuc",
-      d : "stuffX"},
-    parent2:{
-      d :"nope"},
-    parent3:{
-      a : "hans",
-      b : "PEter",
-      c : "hannes"
+// Testing area TODO: refactor this to actual testfiles
+rerootables= ['a', 'b', 'c'];
+dummyobject = {parent: {
+      a: 'Reroot me A',
+      b: 'Reroot me B',
+      c: 'Reroot me C',
+      d: 'Dont reroot me D'},
+    parent2: {
+      d: 'Dont reroot me D'},
+    parent3: {
+      a: 'Reroot me A',
+      b: 'Reroot me B',
+      c: 'Reroot me C',
+      somechild: {
+        a: 'Dont rerootme',
+        b: 'Dont rerootme'},
     },
-     parent4: "parent4"
-}
+     parent4: 'Dont reroot me Parent4',
+};
 
-dummyobject = rerootElements(dummyobject, rerootables, "alph")
-console.log(dummyobject)
+dummyobject = rerootElements(dummyobject, rerootables, 'abc');
+console.log(dummyobject);
